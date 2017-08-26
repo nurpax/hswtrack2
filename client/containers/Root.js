19dfbecb5e15ba5ebbc3bb0fd0d3525b2393a926
Janne Hellsten <jjhellst@gmail.com>
2017-08-26 23:22:01 +0300

    Get rid of /rest/app route and use /rest/weight instead for those purposes

--- client/containers/Root.js
+++ client/containers/Root.js
@@ -5,7 +5,6 @@ import { ConnectedRouter } from 'react-router-redux'
 
 import { userIsNotAuthenticatedRedir, userIsAuthenticatedRedir } from '../auth'
 import Main from './Main'
-import TodoList from './TodoList'
 import Login from './Login'
 import SignUp from './SignUp'
 import Profile from './Profile'
@@ -16,7 +15,6 @@ const { store, history } = configureStore()
 
 const AuthLogin = userIsNotAuthenticatedRedir(Login)
 const AuthSignUp = userIsNotAuthenticatedRedir(SignUp)
-const AuthTodoList = userIsAuthenticatedRedir(TodoList)
 const AuthProfile = userIsAuthenticatedRedir(Profile)
 
 const Root = () => (
@@ -24,7 +22,6 @@ const Root = () => (
     <ConnectedRouter history={history}>
       <Switch>
         <Route exact path='/' component={Main} />
-        <Route exact path='/todos' component={AuthTodoList} />
         <Route exact path='/login' component={AuthLogin} />
         <Route exact path='/profile' component={AuthProfile} />
         <Route exact path='/signup' component={AuthSignUp} />
